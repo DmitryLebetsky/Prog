37eb874f166faeb111981fe2bd299e9d4cab5d5f
Dmitry Lebetsky <dlebetsky@gmail.com>
2022-03-24 10:04:02 +0300

    feat: add task1_lab1

---
+++ lab1/task1/_153501_Lebetsky/_153501_Lebetsky/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _153501_Lebetsky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a / b;
+            Console.WriteLine("a / b = {0}", c);
+        }
+    }
+}
